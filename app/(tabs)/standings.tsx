ca87feb2a04b0f0c2de2a9c24ff231b7da7f3c89
Bartu Gençcan <bartugenccandev@gmail.com>
2024-06-01 15:00:49 +0300

    Standings page done

--- app/(tabs)/standings.tsx
+++ app/(tabs)/standings.tsx
@@ -1,36 +1,84 @@
-import { Text, View, FlatList } from 'react-native';
+import { useEffect, useState, useRef } from 'react';
+import { View, FlatList, StyleSheet, Dimensions } from 'react-native';
 import { Container, DriverCard } from '~/components';
+import { fetchDrivers } from '~/api/get';
+import { DriverRanking } from '~/types/driverRanking.types';
+import LottieView from 'lottie-react-native';
 
-const mockData = [
-  {
-    id: 1,
-    name: 'Lewis Hamilton',
-    number: 44,
-    team: 'Mercedes',
-    image: 'https://media.api-sports.io/formula-1/drivers/20.png',
-    teamLogo: 'https://media.api-sports.io/formula-1/teams/5.png',
-  },
-];
+const { width, height } = Dimensions.get('window');
 
 const Standings = () => {
+  const [drivers, setDrivers] = useState<DriverRanking[]>([]);
+  const [loading, setLoading] = useState(true);
+  const animation = useRef<LottieView>(null);
+
+  useEffect(() => {
+    setLoading(true);
+
+    if (animation.current) {
+      animation.current.play();
+    }
+
+    fetchDrivers().then((data) => {
+      if (data) {
+        setDrivers(data?.response);
+        setLoading(false);
+
+        if (animation.current) {
+          animation.current.pause();
+        }
+      }
+    });
+  }, []);
+
   return (
     <Container>
+      {loading && (
+        <View style={styles.animationContainer}>
+          <LottieView
+            ref={animation}
+            loop
+            autoPlay
+            style={styles.animation}
+            source={require('~/assets/animations/checkered-flag.json')}
+          />
+        </View>
+      )}
+
       <FlatList
-        data={mockData}
-        renderItem={({ item }) => (
+        data={drivers}
+        renderItem={({ item, index }) => (
           <DriverCard
-            key={item.id}
-            name={item?.name}
-            number={item?.number}
+            key={item?.driver?.id}
+            name={item?.driver?.name}
+            number={item?.driver?.number}
             team={item?.team}
-            image={item?.image}
-            teamLogo={item?.teamLogo}
+            image={item?.driver?.image}
+            teamLogo={item?.team?.logo}
+            index={index}
           />
         )}
-        contentContainerStyle={{ flex: 1, alignItems: 'center' }}
+        contentContainerStyle={{ alignItems: 'center' }}
+        ItemSeparatorComponent={() => <View style={{ height: 14 }} />}
+        scrollEnabled
       />
     </Container>
   );
 };
+
+const styles = StyleSheet.create({
+  animationContainer: {
+    flex: 1,
+    width: 250,
+    height: 250,
+    alignItems: 'center',
+    justifyContent: 'center',
+    alignSelf: 'center',
+  },
+  animation: {
+    width: '100%',
+    height: '100%',
+  },
+});
 
 export default Standings;
--- components/DriverCard.tsx
+++ components/DriverCard.tsx
@@ -1,52 +1,81 @@
 import { View, Text, StyleSheet, Dimensions, Image } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 
+// Types
+import { Team } from '~/types/driverRanking.types';
+
+// Constants
+import { teamColors } from '~/constants/TeamColors';
+
+// Animation
+import Animated, { BounceInRight } from 'react-native-reanimated';
+
 interface IDriverCardProps {
   name: string;
   number: number;
-  team: string;
+  team: Team;
   image: string;
   teamLogo: string;
+  index: number;
 }
 
 const { width, height } = Dimensions.get('window');
 const cardHeight = height * 0.2;
 
-export const DriverCard: React.FC<IDriverCardProps> = ({ name, number, team, image, teamLogo }) => {
+export const DriverCard: React.FC<IDriverCardProps> = ({
+  name,
+  number,
+  team,
+  image,
+  teamLogo,
+  index,
+}) => {
   const seperatedName = name.split(' ');
   const firstName = seperatedName[0];
   const lastName = seperatedName[1];
+  const teamColor = teamColors[team.name] || '#DC0000';
 
   return (
-    <LinearGradient
-      start={{ x: 1, y: 0 }}
-      end={{ x: 0, y: 0 }}
-      style={styles.container}
-      colors={['rgba(0,0,0,0.7)', 'transparent']}>
-      <View style={styles.driverInfo}>
-        <Text style={styles.driverNumber}>{number}</Text>
-        <View>
-          <Text style={styles.driverFirstName}>{firstName}</Text>
-          <Text style={styles.driverLastName}>{lastName}</Text>
+    <Animated.View entering={BounceInRight.delay(400 * (index + 1))}>
+      <LinearGradient
+        start={{ x: 1, y: 0 }}
+        end={{ x: 0, y: 0 }}
+        style={[styles.container, { backgroundColor: teamColor }]}
+        colors={['rgba(0,0,0,0.7)', 'transparent']}>
+        <View style={styles.driverInfo}>
+          <Text style={styles.driverNumber}>{number}</Text>
+          <View>
+            <Text style={styles.driverFirstName}>{firstName}</Text>
+            <Text style={styles.driverLastName}>{lastName}</Text>
+          </View>
         </View>
-      </View>
-      <View style={styles.imageContainer}>
-        <Image source={{ uri: image }} resizeMode="cover" height={160} width={140} />
-      </View>
-    </LinearGradient>
+        <View style={styles.imageContainer}>
+          <Image source={{ uri: image }} resizeMode="cover" height={160} width={140} />
+        </View>
+        <Image source={{ uri: teamLogo }} style={styles.teamLogo} resizeMode="contain" />
+      </LinearGradient>
+    </Animated.View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    width: width * 0.9,
+    width: width * 0.92,
     height: cardHeight,
-    borderRadius: 12,
+    borderRadius: 18,
     paddingTop: 10,
     paddingLeft: 10,
     flexDirection: 'row',
     justifyContent: 'space-between',
-    backgroundColor: '#108a7f',
+    shadowColor: '#fff',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    position: 'relative',
   },
   driverInfo: {
     marginLeft: 20,
@@ -70,5 +99,17 @@ const styles = StyleSheet.create({
   },
   imageContainer: {
     alignSelf: 'flex-end',
+  },
+  image: {
+    aspectRatio: 1,
+  },
+  teamLogo: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    width: width * 0.5,
+    height: cardHeight * 0.4,
+    opacity: 0.2,
+    transform: [{ translateX: -width * 0.25 }, { translateY: -cardHeight * 0.25 }],
   },
 });
